ec5e0eb08fe6e7ba8dd05025188c43d6eb60c00e
phamtanphat <phatdroid94@gmail.com>
2023-06-17 19:34:44 +0700

    finish

--- lib/demo_proxy_provider.dart
+++ lib/demo_proxy_provider.dart
@@ -3,6 +3,8 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+import 'package:provider/provider.dart';
+
 class Counter {
   var number;
 
@@ -58,16 +60,81 @@ class ModelRepository {
 }
 
 class DemoProxyproviderWidget extends StatefulWidget {
-  const DemoProxyproviderWidget({Key? key}) : super(key: key);
 
   @override
   State<DemoProxyproviderWidget> createState() => _DemoProxyproviderWidgetState();
 }
 
 class _DemoProxyproviderWidgetState extends State<DemoProxyproviderWidget> {
+
+  ModelRepository? modelRepository;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo proxy provider"),
+      ),
+      body: MultiProvider(
+        providers: [
+          Provider(create: (context) => Counter(number: 0)),
+          ProxyProvider<Counter, CounterManager>(
+            create: (context) => CounterManager(),
+            update: (context, counter, counterManager) {
+              counterManager?.updateCounter(counter: counter);
+              return counterManager ?? CounterManager();
+            }
+          ),
+          ProxyProvider<CounterManager, ModelRepository>(
+              create: (context) => ModelRepository(),
+              update: (context, counterManager, modelRepository) {
+                modelRepository?.updateCounterManager(counterManager: counterManager);
+                return modelRepository ?? ModelRepository();
+              }
+          )
+        ],
+        child: ProxyContainerWidget(),
+      )
+    );
   }
 }
+
+class ProxyContainerWidget extends StatefulWidget {
+  const ProxyContainerWidget({Key? key}) : super(key: key);
+
+  @override
+  State<ProxyContainerWidget> createState() => _ProxyContainerWidgetState();
+}
+
+class _ProxyContainerWidgetState extends State<ProxyContainerWidget> {
+
+  ModelRepository? modelRepository;
+  Counter? counter;
+
+  @override
+  void initState() {
+    super.initState();
+    modelRepository = context.read();
+    counter = context.read();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ElevatedButton(
+        onPressed: () {
+          modelRepository?.calculateIncrease();
+          print(counter?.number.toString());
+        },
+        child: Text("Increase"),
+      ),
+    );
+  }
+}
+
 
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter_provider_25042023/demo_listenable_provider.dart';
 import 'package:flutter_provider_25042023/demo_provider.dart';
 import 'package:flutter_provider_25042023/demo_valuelistenable_provider.dart';
 
+import 'demo_proxy_provider.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -22,8 +24,9 @@ class MyApp extends StatelessWidget {
         "/demo_provider": (context) => DemoProvider(),
         "/demo_listenable_provider": (context) => DemoListenableProvider(),
         "/demo_value_listenable_widget": (context) => DemoValueListenableWidget(),
+        "/demo_proxy_provider": (context) => DemoProxyproviderWidget(),
       },
-      initialRoute: "/demo_value_listenable_widget",
+      initialRoute: "/demo_proxy_provider",
     );
   }
 }
